8f5c6693c4abfec73ea1c279098d2c8a578c4e93
koushingnu <koushin1022apple@gmail.com>
2025-05-09 18:02:05 +0900

    ヘッダーフッターの修正後

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,7 @@
 "use client";
-
 import React from "react";
-import ClientApp from "./ClientApp"; // ← ヘッダー／フッターを出すラッパー
 import ReviewsDashboard from "./ReviewsDashboard";
 
 export default function Page() {
-  return (
-    <ClientApp>
-      <ReviewsDashboard />
-    </ClientApp>
-  );
+  return <ReviewsDashboard />;
 }
